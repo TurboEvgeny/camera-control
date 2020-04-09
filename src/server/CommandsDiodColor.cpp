4a9d82e814ac36bcf98e65c7fc54aef943ed4da7
Evgeniy Bunya <bunya.evgeniy@gmail.com>
2020-04-09 19:25:40 +0300

    server - команды для управления цветом диода

---
+++ src/server/CommandsDiodColor.cpp
@@ -0,0 +1,61 @@
+#include "CommandsDiodColor.h"
+#include "DiodColor.h"
+// класс команды включения-выключения диода
+SetDiodColorCommand::SetDiodColorCommand(DiodColor& colorRef):
+  ICommand(),
+  color(colorRef)
+{
+}
+
+SetDiodColorCommand::~SetDiodColorCommand()
+{
+}
+
+std::string SetDiodColorCommand::getName()
+{
+    return "set-led-color";
+}
+
+std::string SetDiodColorCommand::execute(std::string arg)
+{
+    if (arg.compare("red") == 0)
+    {
+        color.setValue(DiodPalette::RED);
+        return "OK";
+    }
+    else if (arg.compare("green") == 0)
+    {
+        color.setValue(DiodPalette::GREEN);
+        return "OK";
+    }
+    else if (arg.compare("blue") == 0)
+    {
+        color.setValue(DiodPalette::BLUE);
+        return "OK";
+    }
+    else
+    {
+        return "FAILED";
+    }
+}
+
+// класс команды включения-выключения диода
+GetDiodColorCommand::GetDiodColorCommand(const DiodColor& colorRef):
+  ICommand(),
+  color(colorRef)
+{
+}
+
+GetDiodColorCommand::~GetDiodColorCommand()
+{
+}
+
+std::string GetDiodColorCommand::getName()
+{
+    return "get-led-color";
+}
+
+std::string GetDiodColorCommand::execute(std::string arg)
+{
+    return ("OK" + this->color.getValueString());
+}
---
+++ src/server/CommandsDiodColor.h
@@ -0,0 +1,31 @@
+#include "ICommand.h"
+// forward declaration of references
+class DiodColor;
+// класс команды смены цвета диода
+class SetDiodColorCommand : public ICommand
+{
+public:
+    SetDiodColorCommand(DiodColor& colorRef);
+    ~SetDiodColorCommand();
+    // имя команды
+    std::string getName() override;
+    // выполнить команду с аргументами
+    std::string execute(std::string arg) override;
+private:
+    // ссылка на данные о цвете диода
+    DiodColor& color;
+};
+// класс команды получения текущего цвета диода
+class GetDiodColorCommand : public ICommand
+{
+ public:
+    GetDiodColorCommand(const DiodColor& colorRef);
+    ~GetDiodColorCommand();
+    // имя команды
+    std::string getName() override;
+    // выполнить команду с аргументами
+    std::string execute(std::string arg) override;
+ private:
+    // ссылка на данные о состоянии диода
+    const DiodColor& color;
+};
