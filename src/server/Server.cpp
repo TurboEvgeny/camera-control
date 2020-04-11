e34e711940dbade8bf7472cc33ea77ed9cea2fca
Evgeniy Bunya <bunya.evgeniy@gmail.com>
2020-04-11 19:53:23 +0300

    Server - добавлены все команды

--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -1,13 +1,21 @@
 #include "Server.h"
 #include "ICommand.h"
 #include "Camera.h"  // CameraDiod
-#include "CommandsDiodState.h" // SetDiodStateCommand
+#include "CommandsDiodState.h" // SetDiodStateCommand, GetDiodStateCommand
+#include "CommandsDiodColor.h" // SetDiodColorCommand, GetDiodColorCommand
+#include "CommandsDiodFreq.h" // SetDiodFreqCommand, GetDiodFreqCommand
 #include <vector>
 Server::Server()
 {
     pDiod = new CameraDiod();
     // активация команд
     activateCommand(new SetDiodStateCommand(pDiod->state));
+    activateCommand(new GetDiodStateCommand(pDiod->state));
+    activateCommand(new SetDiodColorCommand(pDiod->color));
+    activateCommand(new GetDiodColorCommand(pDiod->color));
+    activateCommand(new SetDiodFreqCommand(pDiod->frequency));
+    activateCommand(new GetDiodFreqCommand(pDiod->frequency));
+
 }
 Server::~Server()
 {
