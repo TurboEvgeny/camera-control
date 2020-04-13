cc834498175b9448c04740acfeab6dc807e97dd5
Evgeniy Bunya <bunya.evgeniy@gmail.com>
2020-04-14 00:18:14 +0300

    клиент - обработка отсутствия сервера

--- src/client/TcpClient.cpp
+++ src/client/TcpClient.cpp
@@ -5,7 +5,23 @@ TcpClient::TcpClient(std::string ip, std::string port):
   s(io_context),
   resolver(io_context)
 {
-  boost::asio::connect(s, resolver.resolve(ip, port));
+    // соединяемся - о результатах сообщаем
+    boost::system::error_code error;
+    boost::asio::connect(s, resolver.resolve(ip, port), error);
+    if (!error)
+    {
+        this->connected = true;
+        // выводим приветствие
+        std::cout << "Enter command or type 'exit'" << std::endl;
+    }
+    // не смогли соединиться, выставляем флаг и пишем сообщение об ошибке
+    else
+    {
+        this->connected = false;
+        std::cout << "FAILED" << std::endl;
+        std::cout << "server unreachable" << std::endl;
+        std::cout << "transmission closed" << std::endl;
+    }
 }
 
 TcpClient::~TcpClient()
@@ -14,27 +30,49 @@ TcpClient::~TcpClient()
 
 void TcpClient::transmission()
 {
-    while(true)
+    while(this->connected)
     {
+        // просим ввести сообщение для сервера 
         std::cout << "Enter message: ";
-        char request[BUFFER_LENGTH];
-        std::cin.getline(request, BUFFER_LENGTH);
+        char request[MAX_BUFFER_LENGTH];
+        std::cin.getline(request, MAX_BUFFER_LENGTH);
         size_t request_length = std::strlen(request);
+        // инициализруем объект обработки ошибок записи
+        boost::system::error_code error_write;
+        // высылываем его
         boost::asio::write(
                            s,
-                           boost::asio::buffer(request, request_length));
-        char reply[BUFFER_LENGTH];
-        boost::system::error_code error;
-        memset(reply, 0, sizeof(reply));
-        // делаем блокирующее считывание данных
-        s.read_some(boost::asio::buffer(reply), error);
-        if (error)
+                           boost::asio::buffer(request, request_length),
+                           error_write);
+        if (error_write)
         {
-            std::cout << "error!";
+            std::cout << "FAILED" << std::endl;
+            std::cout << "check server - out message fail " << std::endl;
+            std::cout << "transmission closed" << std::endl;
+            break;
         }
-        else
+        // создаем буфер для ответа
+        char reply[MAX_BUFFER_LENGTH];
+        // инициалируем буфер нулями
+        memset(reply, 0, sizeof(reply));
+        // объект обработки ошибок чтения
+        boost::system::error_code error_read;
+        // делаем блокирующее считывание данных
+        s.read_some(boost::asio::buffer(reply), error_read);
+        // если ошибка - выводим сообщение об ошибке
+        if (!error_read)
         {
             std::cout << "Reply is: " << reply << std::endl;
         }
+        else
+        {
+            std::cout << "FAILED" << std::endl;
+            std::cout << "input message failure" << std::endl;
+            std::cout << "transmission closed" << std::endl;
+            // выходим из цикла - прием сообщения поломался
+            break;
+        }
     }
+    // вышли из цикла -соединение завершилось
+    this->connected = false;
 }
--- src/client/TcpClient.h
+++ src/client/TcpClient.h
@@ -1,6 +1,6 @@
 #include <boost/asio.hpp>
 #include <string>
-#define BUFFER_LENGTH 1024
+#define MAX_BUFFER_LENGTH 1024
 using boost::asio::ip::tcp;
 // класс клиент с логикой
 class TcpClient
@@ -10,6 +10,9 @@ class TcpClient
   ~TcpClient();
   void transmission();
  private:
+    // признак успешнего соединения
+    bool connected;
+    // boost-овые перенные для tcp-соединения
     boost::asio::io_context io_context;
     tcp::socket s;
     tcp::resolver resolver;
